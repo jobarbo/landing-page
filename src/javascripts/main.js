ec4a4bb3417421b0228a811995bdb02b1e189518
Jonathan barbeau <barbeau.jonathan@gmail.com>
2020-02-26 21:49:36 -0500

    initialising scroll

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -7,7 +7,6 @@ const body = document.querySelector('body');
 const grid = document.querySelector('.artists-grid');
 
 function changeBackground() {
-  console.log('changeBackground()');
   const slideInAt = (window.scrollY + window.innerHeight) - grid.offsetHeight;
   const isHalfShown = slideInAt >= grid.offsetTop;
 
@@ -39,7 +38,7 @@ function debounce(func, wait, immediate) {
 }
 /* eslint-enable */
 
-grid.addEventListener('scroll', debounce(changeBackground, 1, true));
+window.addEventListener('scroll', debounce(changeBackground, 1, true));
 
 // ======================== //
 
@@ -57,3 +56,31 @@ menuBtn.addEventListener('click', () => {
 
 // PARALLAX SCROLL //
 // ======================== //
+
+const picture = document.querySelector('.loud .floating-image');
+
+function onScroll() {
+  let startPos;
+  const speed = picture.dataset;
+  console.log(speed);
+
+  function animateParallax() {
+    const fromTop = window.pageYOffset;
+    const rect = picture.getBoundingClientRect();
+    let scrollDelta = '';
+    if ((rect.top - window.innerHeight) <= 0 && rect.bottom > 0) {
+      if (startPos === undefined) {
+        startPos = fromTop;
+      }
+    } else {
+      startPos = 0;
+      return;
+    }
+    scrollDelta = (fromTop - startPos / 1.25) * 1;
+    picture.style.transform = `translateY(${scrollDelta}px)`;
+  }
+  animateParallax();
+  return animateParallax;
+}
+
+window.addEventListener('scroll', debounce(onScroll, 1, true));
